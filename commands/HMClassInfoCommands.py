f242053f0b5c669d5aa7782b9b38a3574f69dcb8
陈晖茂 <chenhuimao@yeah.net>
2020-06-06 20:25:17 +0800

    [new]fsubclass command

--- commands/HMClassInfoCommands.py
+++ commands/HMClassInfoCommands.py
@@ -5,12 +5,15 @@ An lldb Python script about class information.
 """
 
 import lldb
+import shlex
+import optparse
 import HMLLDBHelpers as HM
 import HMLLDBClassInfo
 
 
 def __lldb_init_module(debugger, internal_dict):
-    debugger.HandleCommand('command script add -f HMClassInfoCommands.findClass fclass -h "Find the class containing the input name"')
+    debugger.HandleCommand('command script add -f HMClassInfoCommands.findClass fclass -h "Find the class containing the input name(Case insensitive)"')
+    debugger.HandleCommand('command script add -f HMClassInfoCommands.findSubclass fsubclass -h "Find the subclass of the input"')
 
 
 def findClass(debugger, command, exe_ctx, result, internal_dict):
@@ -22,6 +25,9 @@ def findClass(debugger, command, exe_ctx, result, internal_dict):
         (lldb) fclass
         (lldb) fclass UITabBarController
         (lldb) fclass controller
+
+    Notice:
+        Case insensitive.
 
     This command is implemented in HMClassInfoCommands.py
     """
@@ -63,3 +69,101 @@ def findClass(debugger, command, exe_ctx, result, internal_dict):
     HM.DPrint(classNames)
 
 
+def findSubclass(debugger, command, exe_ctx, result, internal_dict):
+    """
+    Syntax:
+        fsubclass [--nonrecursively] <className>
+
+    Options:
+        --nonrecursively/-n; Find subclass non-recursively
+
+    Examples:
+        (lldb) fsubclass UIViewController
+        (lldb) fsubclass -n UIViewController
+
+
+    This command is implemented in HMClassInfoCommands.py
+    """
+
+    command_args = shlex.split(command)
+    parser = generate_findSubclass_option_parser()
+    try:
+        # options: optparse.Values
+        # args: list
+        (options, args) = parser.parse_args(command_args)
+    except:
+        result.SetError(parser.usage)
+        return
+
+    if len(args) != 1:
+        HM.DPrint("Requires a argument, Please enter \"help fsubclass\" for help.")
+        return
+
+    HM.DPrint("Waiting...")
+
+    if options.nonrecursively:
+        compareScript = '''
+            if (class_getSuperclass(cls) == inputClass){
+                NSString *name = [[NSString alloc] initWithUTF8String:class_getName(cls)];
+                [result appendString:name];
+                [result appendString:@"\\n"];
+                findCount += 1;
+            }
+        '''
+
+    else:
+        compareScript = '''
+            for (Class superClass = class_getSuperclass(cls); superClass != nil; superClass = class_getSuperclass(superClass)) {
+                if (superClass == inputClass) {
+                    NSString *name = [[NSString alloc] initWithUTF8String:class_getName(cls)];
+                    [result appendString:name];
+                    [result appendString:@"\\n"];
+                    findCount += 1;
+
+                    break;
+                }
+            }
+        '''
+
+    command_script = f'''
+        Class inputClass = objc_lookUpClass("{args[0]}");
+        NSMutableString *result = [[NSMutableString alloc] init];
+
+        if (inputClass == nil) {{
+            [result appendString:@"Can't find {args[0]} class"];
+        }} else {{
+            unsigned int classCount;
+            unsigned int findCount = 0;
+            Class *classList = objc_copyClassList(&classCount);
+
+            for (int i = 0; i < classCount; i++) {{
+                Class cls = classList[i];
+                {compareScript}
+            }}
+
+            if (findCount == 0) {{
+                [result insertString:@"No subclass found." atIndex:0];
+            }} else {{
+                [result insertString:[[NSString alloc] initWithFormat:@"Subclass count: %u \\n", findCount] atIndex:0];
+            }}
+            
+            free(classList);
+        }}
+
+        result;
+    '''
+
+    classNames = HM.evaluateExpressionValue(command_script).GetObjectDescription()
+    HM.DPrint(classNames)
+
+
+def generate_findSubclass_option_parser() -> optparse.OptionParser:
+    usage = "usage: fsubclass [-n] <className>"
+    parser = optparse.OptionParser(usage=usage, prog="fsubclass")
+    parser.add_option("-n", "--nonrecursively",
+                      action="store_true",
+                      default=False,
+                      dest="nonrecursively",
+                      help="Find subclass non-recursively")
+
+    return parser
