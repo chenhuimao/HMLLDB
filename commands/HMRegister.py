271cc4007171d9c2f47692d647fb1ed5f23ed981
chenhuimao <chenhuimao@yeah.net>
2022-07-28 21:14:53 +0800

    [optimize]'rc' command: print last instruction if needed

--- commands/HMRegister.py
+++ commands/HMRegister.py
@@ -23,14 +23,15 @@
 # https://github.com/chenhuimao/HMLLDB
 
 import lldb
-from typing import Dict
+from typing import Dict, List
 import shlex
 import optparse
 import HMLLDBHelpers as HM
 import HMLLDBClassInfo
 
-# [name, value]
+# [register_name, register_value]
 g_last_registers_dict: Dict[str, str] = {}
+last_disassemble: str = ""
 
 
 def __lldb_init_module(debugger, internal_dict):
@@ -63,11 +64,20 @@ def register_change(debugger, command, exe_ctx, result, internal_dict):
         return
 
     frame = exe_ctx.GetTarget().GetProcess().GetSelectedThread().GetSelectedFrame()
-    current_registers: lldb.SBValueList = frame.GetRegisters()
     global g_last_registers_dict
     if len(g_last_registers_dict) == 0:
         HM.DPrint("Get register for the first time.")
 
+    # Is it repeated?
+    if is_executed_repeatedly(frame):
+        HM.DPrint("Executed repeatedly!")
+        return
+
+    # When the pc registers differ by 4
+    print_last_instruction_if_needed(frame)
+
+    # Print and save registers
+    current_registers: lldb.SBValueList = frame.GetRegisters()
     for register_set_value in current_registers:
         is_print: bool = options.all or "General Purpose Registers" == register_set_value.GetName()
         has_print_register_set_name: bool = False
@@ -83,10 +93,14 @@ def register_change(debugger, command, exe_ctx, result, internal_dict):
             if is_print and reg_value.GetValue() != last_register_value:
                 if not has_print_register_set_name:
                     has_print_register_set_name = True
-                    HM.DPrint(f"{register_set_value.GetName()}:")
-                HM.DPrint(f"\t\t{reg_value.GetName()}:{last_register_value} -> {reg_value.GetValue()}")
+                    print(f"{register_set_value.GetName()}:")
+                print(f"\t\t{reg_value.GetName()}:{last_register_value} -> {reg_value.GetValue()}")
 
             g_last_registers_dict[reg_value.GetName()] = reg_value.GetValue()
+
+    # Record last disassemble
+    global last_disassemble
+    last_disassemble = frame.Disassemble()
 
 
 def generate_register_change_option_parser() -> optparse.OptionParser:
@@ -98,4 +112,40 @@ def generate_register_change_option_parser() -> optparse.OptionParser:
                       dest="all",
                       help="Show all register sets.")
     return parser
+
+
+def is_executed_repeatedly(frame: lldb.SBFrame) -> bool:
+    last_pc_value: int = 0
+    if "rip" in g_last_registers_dict:
+        last_pc_value = int(g_last_registers_dict["rip"], 16)
+    if "pc" in g_last_registers_dict:
+        last_pc_value = int(g_last_registers_dict["pc"], 16)
+    return frame.GetPC() == last_pc_value
+
+
+def print_last_instruction_if_needed(frame: lldb.SBFrame) -> None:
+    pc_key = "pc"
+    global g_last_registers_dict
+    if pc_key not in g_last_registers_dict:
+        return
+    last_pc_value = int(g_last_registers_dict[pc_key], 16)
+    if frame.GetPC() - last_pc_value != 4:
+        return
+
+    global last_disassemble
+    instruction_list: List[str] = last_disassemble.splitlines(False)
+    for instruction_line in instruction_list:
+        instruction_line_strip = instruction_line.lstrip("->").strip()
+        instruction_line_split: List[str] = instruction_line_strip.split(" ")
+        address: str = ""
+        for element in instruction_line_split:
+            if element.startswith("0x"):
+                address = element
+                break
+        if not address.startswith("0x"):
+            continue
+        address = address.rstrip(":")
+        if int(address, 16) == last_pc_value:
+            print(instruction_line_strip)
+            break
 
